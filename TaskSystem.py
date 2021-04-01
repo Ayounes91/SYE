f1535b7a7ea810ba7a724ca8ef783fe039bb891e
chrislvr <cmayika17@gmail.com>
2021-04-01 11:35:42 +0200

    Add files via upload

--- TaskSystem.py
+++ TaskSystem.py
@@ -8,6 +8,58 @@ Created on Wed Mar 31 20:02:02 2021
 from Task import Task
 
 class TaskSystem:
+    def __init__(self,liste,dic):
+        self.liste=liste
+        self.dic=dic
+    
+    def TaskList():
+        liste =[]
+        rep = None
+        print("Création d'un système de tâches.\n")
+        while(rep!="0"):
+            liste.append(Task.newTask())
+            print("Liste de tâches : ")
+            for k in liste:
+                print(k.description())
+            rep = input("Voulez-vous créer une autre tâche ? (1/0)")    
+            while(rep not in ["0","1"]):
+                rep = input("Voulez-vous créer une autre tâche ? (1/0)")
+        print("Crétion du système de tâches terminé.")
+        return liste
+    
+    def getDependencies(t,liste):
+        dependance=[]
+        for k in liste:
+            if(t.name!=k.name):
+                if(TaskSystem.interference(t,k)):
+                    if(liste.index(t)>liste.index(k)):
+                        dependance.append(k)
+        print("Dépendances\n")
+        print(t.name+" : ")
+        for j in dependance:
+            print(j.name+", ")
+        return dependance
+    
+    def dic(liste):
+        dic={}
+        for k in liste:
+            dic[k.name]=TaskSystem.getDependencies(k,liste).name
+        print(dic)
+        return dic   
+    
+    def creerSysteme():
+        liste = TaskSystem.TaskList()
+        dictionnaire = TaskSystem.dic(liste)
+        return TaskSystem(liste,dictionnaire)
+        
+                
+        
+        
+            
+           
+           
+        
+    
     def interference1(t1,t2):
         boo = False
         for i in t1.reads:
--- main.py
+++ main.py
@@ -8,8 +8,4 @@ Created on Wed Mar 31 18:22:34 2021
 from Task import Task
 from TaskSystem import TaskSystem
 
-t1 = Task.newTask()
-t2 = Task.newTask()
-t1.description()
-t2.description()
-TaskSystem.inter(t1,t2)
+s1 = TaskSystem.creerSysteme()
